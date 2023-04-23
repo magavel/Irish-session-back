8ce37c9cf9b29545858b92aa69742b2d26433d7c
Avel FERON PAPEL <avel.feron@atos.net>
2023-04-23 22:02:01 +0200

    first commit

---
+++ src/api/tune/controllers/tune.ts
@@ -0,0 +1,7 @@
+/**
+ * tune controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tune.tune');
---
+++ src/api/tune/routes/tune.ts
@@ -0,0 +1,7 @@
+/**
+ * tune router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tune.tune');
